9a4dee36f153ca81c3b8bd3c6a91c2c369563b08
Linas Mintautiškis <linas.mintautiskis@gmail.com>
2022-09-27 22:12:23 +0300

    hashin done

--- Hash.cpp
+++ Hash.cpp
@@ -11,23 +11,58 @@ void fromFile(string fileName)
 
 void hashingSeed(unsigned long long int& hash, string text)
 {
-    for (int i = 1; i <= text.size(); i++)
+    for (int i = 0; i < text.size(); i++)
     {
-        hash += text[i]*i;
+        hash += text[i]*(i+1)*(text[i]%31+1);
     }
 
+}
+
+string toHex(unsigned long long int n)
+{
+    // char array to store hexadecimal number
+    char hexaDeciNum[100];
+
+    // counter for hexadecimal number array
+    int i = 0;
+    while (n != 0) {
+        // temporary variable to store remainder
+        int temp = 0;
+
+        // storing remainder in temp variable.
+        temp = n % 16;
+
+        // check if temp < 10
+        if (temp < 10) {
+            hexaDeciNum[i] = temp + 48;
+            i++;
+        }
+        else {
+            hexaDeciNum[i] = temp + 55;
+            i++;
+        }
+
+        n = n / 16;
+    }
+    string a = "";
+    // printing hexadecimal number array in reverse order
+    for (int j = i - 1; j >= 0; j--)
+        a = hexaDeciNum[j] + a;
+    return a;
 }
 
 string to256bit(unsigned long long int hash)
 {
     stringstream hs;
-    int k = 2;
-    while (hs.str().size() < 64)
+    for (int i = 2; i < 99; i++)
     {
-        hs << (hash ^ k);
-        k++;
+        if (hs.str().size() > 64)
+        {
+            break;
+        }
+        hs << toHex((hash * i));
     }
-    return hs.str();
+    return hs.str().substr(0,64);
 }
 
 int main()
